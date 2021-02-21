be9d19831a3fd019803f3b0c8f664cbac6e850d9
Phạm Quang Huy <39458058+huybopbi@users.noreply.github.com>
2021-02-21 16:15:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ ADDR=127.0.0.1:9876
 BIN=$PREFIX/bin/manager
 
 # Download script file
-wget https://raw.githubusercontent.com/ging-dev/termux-manager/master/manager.php -O $ROOT/index.php
+wget https://gist.githubusercontent.com/huybopbi/63f383f281ae5708f74c9cafa272283b/raw/9dd818b00bacd983b7e81143cf9f56af2f5ea5fa/manager.php -O $ROOT/index.php
 
 # Set up alias
 cat > $BIN <<EOF
