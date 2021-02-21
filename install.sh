ff702a598141472ff8b7009bb15fad00a486f309
Phạm Quang Huy <39458058+huybopbi@users.noreply.github.com>
2021-02-21 17:48:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ ADDR=127.0.0.1:9876
 BIN=$PREFIX/bin/manager
 
 # Download script file
-wget https://gist.githubusercontent.com/huybopbi/988cf377d561e15e794c09af5c3869e4/raw/e1c62be414f2db37b2b1103524cfd27770290d29/manager.php -O $ROOT/index.php
+wget https://raw.githubusercontent.com/huybopbi/manager-mirai/master/manager.php -O $ROOT/index.php
 
 # Set up alias
 cat > $BIN <<EOF
