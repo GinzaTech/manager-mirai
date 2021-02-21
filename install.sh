8e9c2379b98c9c428df4135f938d3f4a9d356ecd
Phạm Quang Huy <39458058+huybopbi@users.noreply.github.com>
2021-02-21 17:30:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ ADDR=127.0.0.1:9876
 BIN=$PREFIX/bin/manager
 
 # Download script file
-wget https://gist.githubusercontent.com/huybopbi/63f383f281ae5708f74c9cafa272283b/raw/9dd818b00bacd983b7e81143cf9f56af2f5ea5fa/manager.php -O $ROOT/index.php
+wget https://gist.githubusercontent.com/huybopbi/988cf377d561e15e794c09af5c3869e4/raw/e1c62be414f2db37b2b1103524cfd27770290d29/manager.php -O $ROOT/index.php
 
 # Set up alias
 cat > $BIN <<EOF
