f18f2a7d8e6fea511b40e19e8bbe212b2bfd670e
Nguyễn Chí Thành Lộc <96804285+GinzaTech@users.noreply.github.com>
2022-03-31 17:44:41 +0700

    Update manager.php

--- manager.php
+++ manager.php
@@ -12,7 +12,7 @@ $CONFIG = '{"lang":"en","error_reporting":false,"show_hidden":false,"hide_Cols":
 define('VERSION', '2.4.3');
 
 //Application Title
-define('APP_TITLE','Gin Manager');
+define('APP_TITLE','GZT Manager');
 
 // --- EDIT BELOW CONFIGURATION CAREFULLY ---
 
